f8d44e5c3ddc7fc729c09b36190f987d9802b557
Nguyen Cong Duy <ncongduy@gmail.com>
2022-01-13 13:57:22 +0200

    feat: display hours when time count down

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-const runButton = document.getElementById("runButton");
-const resetButton = document.getElementById("resetButton");
-const timerText = document.getElementById("timerText");
-const audio = new Audio("./audio/nothing mix adc _01.mp3");
+const runButton = document.getElementById('runButton');
+const resetButton = document.getElementById('resetButton');
+const timerText = document.getElementById('timerText');
+const audio = new Audio('./audio/nothing mix adc _01.mp3');
 let isOngoing = false;
 
 const timerFunc = () => {
-  if (isOngoing) return;
-  const inputMinutes = document.getElementById("typeMinutes").value;
-  const startTimer = new Date().getTime();  
-  const endTimer = startTimer + inputMinutes*60*1000;
+	if (isOngoing) return;
+	const inputMinutes = document.getElementById('typeMinutes').value;
+	const startTimer = new Date().getTime();
+	const endTimer = startTimer + inputMinutes * 60 * 1000;
 
-  //Check timer
-  const checkTimer = (time) => {
-    if (time <= 0) {
-      isOngoing = false;
-      clearInterval(runInterval);
-      audio.play();
-      timerText.innerHTML = `0:0`;
-    }
-  };
+	//Check timer
+	const checkTimer = (time) => {
+		if (time <= 0) {
+			isOngoing = false;
+			clearInterval(runInterval);
+			audio.play();
+			timerText.innerHTML = `00:00:00`;
+		}
+	};
 
-  //Run timer
-  const runInterval = setInterval(() => {
-    isOngoing = true;
-    let currentTimer = new Date().getTime();
-    let countTimer = endTimer - currentTimer;
-    let minutes = Math.floor(countTimer / 60000) % 60;
-    let seconds = Math.floor(countTimer / 1000) % 60;
+	//Run timer
+	const runInterval = setInterval(() => {
+		isOngoing = true;
+		let currentTimer = new Date().getTime();
+		let countTimer = endTimer - currentTimer;
+		let hours = `0${Math.floor(countTimer / 1000 / 60 / 60) % 24}`.slice(
+			-2
+		);
+		let minutes = `0${Math.floor(countTimer / 1000 / 60) % 60}`.slice(-2);
+		let seconds = `0${Math.floor(countTimer / 1000) % 60}`.slice(-2);
 
-    //render minutes and seconds in browser
-    timerText.innerHTML = `${minutes}:${seconds}`;  
+		//render minutes and seconds in browser
+		timerText.innerHTML = `${hours}:${minutes}:${seconds}`;
 
-    //if on time, play music
-    checkTimer(countTimer);      
-  }, 1000);
+		//if on time, play music
+		checkTimer(countTimer);
+	}, 1000);
 };
 
-runButton.addEventListener("click", timerFunc);
-resetButton.addEventListener("click", () => {
-  window.location.reload();
+runButton.addEventListener('click', timerFunc);
+
+resetButton.addEventListener('click', () => {
+	window.location.reload();
 });
